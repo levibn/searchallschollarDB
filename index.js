e9ea55b31fe11ef72cca4682f9aac991a69fd941
Levi Barros Nobrega <lbn@fca.cin.ufpe.br>
2018-02-16 09:44:42 -0300

    adding express

--- index.js
+++ index.js
@@ -2,7 +2,19 @@ var querystring = require('querystring');
 var data = {'query':'heart','count':'2', 'view':'COMPLETE'};
 const https = require('https');
 const request = require('request');
-request('https://api.elsevier.com/content/search/scidir?'+ querystring.stringify(data), { json: true, headers: {'X-ELS-APIKey': '3e8c962f55e8745c45129995d58a4dd3'} }, (err, res, body) => {
-  if (err) { return console.log('erro',err); }
-  console.log('sucesso',body);
-});
+var express = require('express'),
+app = express(),
+port = process.env.PORT || 3000;
+app.get('/', function (req, res) {
+  res.send('Hello World');
+})
+app.get('/teste', function (req, res) {
+  request('https://api.elsevier.com/content/search/scidir?'+ querystring.stringify(data), { json: true, headers: {'X-ELS-APIKey': '3e8c962f55e8745c45129995d58a4dd3'} }, (err, r, body) => {
+    if (err) { return console.log('erro',err); }
+    console.log('sucesso',body);
+    res.send(body);
+  });
+})
+app.listen(port);
+
+console.log('todo list RESTful API server started on: ' + port);
