8ee838d806490b79cdd6c5364633491e97da2757
Levi Barros Nobrega <lbn@fca.cin.ufpe.br>
2018-02-16 09:58:50 -0300

    removing console log

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
 app.get('/teste', function (req, res) {
   request('https://api.elsevier.com/content/search/scidir?'+ querystring.stringify(data), { json: true, headers: {'X-ELS-APIKey': '3e8c962f55e8745c45129995d58a4dd3'} }, (err, r, body) => {
     if (err) { return console.log('erro',err); }
-    console.log('sucesso',body);
+    //console.log('sucesso',body);
     res.send(body);
   });
 })
